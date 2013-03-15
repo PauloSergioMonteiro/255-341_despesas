8c0ffcf88894f8a3e63c688d6005051d79272037
paulo <paulo@paulomonteiro>
2013-03-15 14:30:35 +0000

    teste2

--- EAPLI_PROJECTO/src/eapli_projecto/EAPLI_PROJECTO.java
+++ EAPLI_PROJECTO/src/eapli_projecto/EAPLI_PROJECTO.java
@@ -14,6 +14,6 @@ public class EAPLI_PROJECTO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println ("teste"); 
+        
     }
 }
