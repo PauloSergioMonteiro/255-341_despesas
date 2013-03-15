d7ef49f7972679c8a27052b4344b5e13fbee71fc
João Andres Moniz Rivera <joao2004_93@hotmail.com>
2013-03-15 14:28:07 +0000

    Teste1

--- EAPLI_PROJECTO/src/eapli_projecto/EAPLI_PROJECTO.java
+++ EAPLI_PROJECTO/src/eapli_projecto/EAPLI_PROJECTO.java
@@ -14,6 +14,6 @@ public class EAPLI_PROJECTO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println ("teste"); 
     }
 }
